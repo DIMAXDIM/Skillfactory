f765c531ec77bed85c523a891897729f79a50461
DIMAXDIM <122447154+DIMAXDIM@users.noreply.github.com>
2023-01-19 03:14:22 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,105 @@
+field = [[" "] * 3 for i in range(3) ]
+def field_print() :
+    print(f"  0 1 2")
+    for i in range(3) :
+        row_info = " ".join(field[i])
+        print(f"{i} {row_info}")
+
+
+def ask():
+    while True:
+        cords = input(" Ваш ход: ").split()
+        if len(cords) != 2:
+            print(" Введите 2 координаты! ")
+            continue
+
+
+
+        X, y = cords
+
+
+        if not(x.isdigit()) or not(y.isdigit()):
+            print(" Введите числа! ")
+            continue
+
+        x, y = int(x), int(y)
+
+        if 0 > x or x > 2 or 0 > y or y > 2:
+            print(" Координаты вне диапазона! ")
+            continue
+
+
+        if field[x][y] != "":
+            print(" Клетка занята! ")
+            continue
+            return x, y
+
+
+
+num = 0
+while True:
+    num += 1
+
+    field_print()
+
+
+
+    if num % 2 == 1:
+        print(" Ходит крестик ")
+
+    else:
+        print(" Ходит нолик ")
+
+    x, y = ask()
+    if num% 2 == 1:
+        field[x][y] = "X"
+    else:
+        field[x][y] = "0"
+
+    if num == 9 :
+        break
+        print("Ничья")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
